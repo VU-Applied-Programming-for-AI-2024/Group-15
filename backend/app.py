a63c9297b5b0fda5f6ef2e59bc07465f04c3b135
felixfaruix <felixthesame@gmail.com>
2024-06-11 00:46:17 +0200

    update

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-import os
 from config import config
 
 app = Flask(__name__)
@@ -15,8 +14,7 @@ def home():
 
 @app.route('/api/test')
 def test_api():
-    return jsonify(status='success', message='API is workingg')
+    return jsonify(status='success', message='API is working')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
-
