cac5d305a14e266901351af7ce6c8b703aaaf656
felixfaruix <felixthesame@gmail.com>
2024-06-10 10:45:46 +0200

    ss

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,12 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from dotenv import load_dotenv
 import os
 from config import config
-
-load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# Determine which environment to use
 env = os.environ.get('FLASK_ENV', 'development')
 app.config.from_object(config[env])
 
@@ -21,4 +19,5 @@ def test_api():
     return jsonify(status='success', message='API is working')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
