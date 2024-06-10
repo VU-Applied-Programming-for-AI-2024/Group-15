03e966863e53eee502ab1028d0a4716293dc6523
felixfaruix <felixthesame@gmail.com>
2024-06-10 10:57:22 +0200

    no environment

--- backend/app.py
+++ backend/app.py
@@ -6,9 +6,8 @@ from config import config
 app = Flask(__name__)
 CORS(app)
 
-# Determine which environment to use
-env = os.environ.get('FLASK_ENV', 'production')
-app.config.from_object(config[env])
+# Load configuration
+app.config.from_object(config)
 
 @app.route('/')
 def home():
@@ -19,5 +18,5 @@ def test_api():
     return jsonify(status='success', message='API is working')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8000)
+
--- backend/config.py
+++ backend/config.py
@@ -4,22 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_secure_key'
     EXERCISE_API_KEY = os.environ.get('EXERCISE_API_KEY') or '4623|B0oWv01vaf4fCpyzvGYwrHiWQI1Jh1fy60FbgBrh'
     BASE_URL = os.environ.get('BASE_URL') or "https://zylalabs.com/api/392/exercise+database+api"
-    DEBUG = False
+    DEBUG = True  # Set this to True for simplicity
     TESTING = False
 
-class DevelopmentConfig(Config):
-    DEBUG = True
-    ENV = 'development'
-
-class TestingConfig(Config):
-    TESTING = True
-    ENV = 'testing'
-
-class ProductionConfig(Config):
-    ENV = 'production'
-
-config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig
-}
+config = Config()
