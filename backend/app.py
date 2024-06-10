018a2a396381987e126d0a1b86093c7a5af97ae0
felixfaruix <felixthesame@gmail.com>
2024-06-10 10:31:13 +0200

    env config

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
 import os
 from config import config
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Determine which environment to use
 env = os.environ.get('FLASK_ENV', 'development')
 app.config.from_object(config[env])
 
