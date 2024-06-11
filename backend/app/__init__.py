f31959971f802f61ab9780e7bb1904cc7bbf825d
felixfaruix <felixthesame@gmail.com>
2024-06-11 18:17:46 +0200

    init.py updates, trying to solve deployment

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    
-    @app.route('/')
-    def home():
-        return render_template('home.html')
-
+    from .routes import main
+    app.register_blueprint(main)
     return app
