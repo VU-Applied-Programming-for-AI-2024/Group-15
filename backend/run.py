25a6bdad2e50a884509a16e17da87bba495af22c
felixfaruix <felixthesame@gmail.com>
2024-06-12 07:38:02 +0200

    connectivity set up

--- backend/run.py
+++ backend/run.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from waitress import serve
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "https://icy-rock-0c06feb03.5.azurestaticapps.net"}})
-  
+
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 # Sample data
@@ -27,4 +28,4 @@ def add_item():
     return jsonify(new_item), 201
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=8000)
