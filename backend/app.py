5717fb6d951a6310242267fe9fcd4d078fac6021
felixfaruix <felixthesame@gmail.com>
2024-06-12 18:22:38 +0200

    yeah

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__, static_folder='static', template_folder='templates')
+
+    # Import the blueprint
+    from .routes import main
+
+    # Register the blueprint
+    app.register_blueprint(main)
+
+    return app
+
+create_app()
+
