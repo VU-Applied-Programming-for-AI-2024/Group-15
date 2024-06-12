fdc5e980320069d538124a0d31894780848c4fea
felixfaruix <felixthesame@gmail.com>
2024-06-12 06:31:02 +0200

    Loaded .env variables

--- backend/run.py
+++ backend/run.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from waitress import serve
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!!!!!!!!!"
-    
+    return "Hello, World!"
+
 if __name__ == "__main__":
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=int(os.getenv("WEBSITE_PORT", 8000)))
