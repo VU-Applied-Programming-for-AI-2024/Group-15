51de6276aeb268101c0b46619203ddf92eb58ab5
felixfaruix <felixthesame@gmail.com>
2024-06-12 17:16:33 +0200

    trial

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = create_app()
-CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
