0da1245230d4215968ce2f3149613503403df59f
felixfaruix <felixthesame@gmail.com>
2024-06-12 11:30:40 +0200

    trials

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-import os
+
 
 load_dotenv()
 
@@ -24,5 +24,4 @@ def add_item():
     return jsonify(new_item), 201
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("WEBSITE_PORT", 8080)))
+    app.run(host="0.0.0.0", port=8000)
