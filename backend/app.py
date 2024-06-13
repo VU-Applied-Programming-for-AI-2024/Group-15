949f0ea4cc199dc612b9b151b3c79027e77c39ce
felixfaruix <felixthesame@gmail.com>
2024-06-13 16:49:45 +0200

    m

--- backend/app.py
+++ backend/app.py
@@ -1,32 +1,11 @@
-from flask import Flask, jsonify, request, render_template
-from flask_cors import CORS
-from dotenv import load_dotenv
-
-load_dotenv()
+from flask import Flask
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}}) 
-
-data = [
-    {"id": 1, "name": "Item 1"},
-    {"id": 2, "name": "Item 2"}
-]
-
-@app.route('/api/items', methods=['GET'])
-def get_items():
-    return jsonify(data)
-
-@app.route('/api/items', methods=['POST'])
-def add_item():
-    new_item = request.json
-    data.append(new_item)
-    return jsonify(new_item), 201
 
 @app.route('/')
-def manual_schedule():
-
-    return render_template('manual_schedule.html')
+def home():
+    return "Hello, Azure!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
 
