114fc9927d7cd89ab4e6a1853423d4eef7955ff4
felixfaruix <felixthesame@gmail.com>
2024-06-20 14:24:49 +0200

    savez

--- backend/app/__init__.py
+++
@@ -1,5 +0,0 @@
-def create_app():
-    from flask import Flask
-    app = Flask(__name__)
-    from .routes import register_routes
-   
--- backend/app/app.py
+++ backend/app/app.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+app = Flask(__name__)
+
 
 @app.route('/')
 def create_app():
