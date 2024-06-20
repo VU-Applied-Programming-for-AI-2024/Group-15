6a5f15963ce2c7bead6c848ee1c7f411aa5ba1b7
felixfaruix <felixthesame@gmail.com>
2024-06-20 14:13:02 +0200

    savez

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-from app import routes
-
+def create_app():
+    from flask import Flask
+    app = Flask(__name__)
+    from .routes import register_routes
+   
