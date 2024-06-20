d2395cfe98fc1dd5c1998dae1e518d7eeec15cda
felixfaruix <felixthesame@gmail.com>
2024-06-20 18:20:18 +0200

    savez

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,10 @@
-from run import create_app
+from flask import Flask
+from flask_cors import CORS
+import logging
 
-app = create_app()
+
+app = Flask(__name__)
+CORS(app) 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000)
