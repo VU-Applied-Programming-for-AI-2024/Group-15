ae95bf2e3b8aacf5aef0fde6c0ea474890a1f004
timovd12 <timovd12@gmail.com>
2024-06-22 00:24:06 +0200

    making it a json

--- backend/models/user-info.py
+++ backend/models/user-info.py
@@ -9,7 +9,7 @@ from models.bodypart import BodyPart
 from pymongo import MongoClient
 import pymongo.errors
 from utils.crud_operations_azure import server_crud_operations
-
+import json
 app = Flask(__name__)
 CORS(app)
 
@@ -55,7 +55,9 @@ def gather_info():
                 gender = "female"
 
         custom_schedule = create_custom_schedule(gender, weight, goal, muscle_list, days)
-        
+
+        json_custom_schedule = json.dumps(custom_schedule)
+
         # Insert the custom schedule into MongoDB
         server_crud_operations(
             operation="insert",
