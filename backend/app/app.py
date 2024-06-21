a77bf197dc8c3903a6825e7b8a23b206f53c2fbe
jezza4 <jesseh0457@gmail.com>
2024-06-21 11:05:16 +0200

    deleted init and put logic in app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,16 @@
-from app import create_app
+from flask import Flask
+from flask_cors import CORS
+import logging
+from routes import register_routes  # Import the register_routes function from routes
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    logging.basicConfig(level=logging.INFO)
+    register_routes(app)
+
+    return app
 
 app = create_app()
 
