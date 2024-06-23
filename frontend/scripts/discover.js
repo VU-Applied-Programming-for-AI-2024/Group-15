5f87149411e6069d1dd8473dda42bedc8d011398
jezza4 <jesseh0457@gmail.com>
2024-06-23 16:58:08 +0200

    discover functions test

---
+++ frontend/scripts/discover.js
@@ -0,0 +1,37 @@
+function handleSearch() {
+    const userInput = document.getElementById('searchInput').value;
+    const bodypart = document.getElementById('bodypartSelect').value;
+    const equipment = document.getElementById('equipmentSelect').value;
+
+    fetch('/search_exercises', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user_input: userInput, bodypart: bodypart, equipment: equipment })
+    })
+    .then(response => response.json())
+    .then(data => displayResults(data))
+    .catch(error => console.error('Error:', error));
+}
+
+function displayResults(data) {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '';
+
+    if (data.error) {
+        resultsDiv.textContent = data.error;
+    } else {
+        data.forEach(exercise => {
+            const exerciseElement = document.createElement('div');
+            exerciseElement.innerHTML = `
+                <p><strong>${exercise.name}</strong></p>
+                <img src="${exercise.gifUrl}" alt="${exercise.name}">
+                <p>Equipment: ${exercise.equipment}</p>
+                <p>Body Part: ${exercise.bodyPart}</p>
+                <p>Target: ${exercise.target}</p>
+            `;
+            resultsDiv.appendChild(exerciseElement);
+        });
+    }
+}
