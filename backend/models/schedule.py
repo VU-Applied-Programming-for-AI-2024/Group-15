2fa93fa8bb7f759fa71ae954ede528d2714b1fae
timovd12 <timovd12@gmail.com>
2024-06-24 13:11:29 +0200

    reverting changes

--- backend/models/schedule.py
+++ backend/models/schedule.py
@@ -1,8 +1,4 @@
-import re
-from day import Day
-from workout import Workout
-from workout_exercise import WorkoutExercise
-from exercise import Exercise
+
 class Schedule:
     def __init__(self, routine):
         self.schedule = {day: None for day in Day}
